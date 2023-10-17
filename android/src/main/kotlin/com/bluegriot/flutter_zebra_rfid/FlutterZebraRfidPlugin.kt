4752b1a6a4c00cbacb9aeb1e408e2d6cf1e2d972
guntungpr <guntungblinkerz123@gmail.com>
2023-10-17 10:57:10 +0700

    first committ

---
+++ android/src/main/kotlin/com/bluegriot/flutter_zebra_rfid/FlutterZebraRfidPlugin.kt
@@ -0,0 +1,34 @@
+package com.bluegriot.flutter_zebra_rfid
+
+import android.content.Context
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+
+
+/** FlutterZebraRfidPlugin */
+class FlutterZebraRfidPlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+  private lateinit var rfidManager : RfidManager
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_zebra_rfid")
+    channel.setMethodCallHandler(this)
+    rfidManager = RfidManager(channel, flutterPluginBinding.applicationContext)
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
+    rfidManager.onMethodCall(call, result)
+  }
+
+}
---
+++ lib/reader.dart
@@ -0,0 +1,234 @@
+part of flutter_zebra_rfid;
+
+/// Represent a RFID reader
+class Reader extends Object {
+
+  /// The reader's identifier
+  final String id;
+
+  ///The reader's name
+  String name;
+
+  /// Indicates if the reader is active (connected)
+  bool get isActive => _isActive;
+  bool _isActive = false;
+
+  late StreamController<TagData> _tagReadStreamController;
+  Stream<TagData> get tagReadingStream  => _tagReadStreamController.stream;
+
+  bool _isTriggerPressed = false;
+  bool get isTriggerPressed => _isTriggerPressed;
+  late StreamController<bool> _readerTriggerController;
+  Stream<bool> get readerTriggerStream => _readerTriggerController.stream;
+
+  bool _isInventoring = false;
+  bool get isInventoring => _isInventoring;
+  late StreamController<bool> _readerInventoringController;
+  Stream<bool> get readerInventoringStream => _readerInventoringController.stream;
+
+  bool _isLocating = false;
+  bool get isLocating => _isLocating;
+  late StreamController<int> _proximityController;
+  Stream<int> get proximityStream => _proximityController.stream;
+  Completer<void>? _connectionCompleter;
+
+  void Function()? onDisconnectedHandler;
+
+  Reader({
+    required this.id, 
+    required this.name
+  });
+
+  void dispose() async { 
+    await _tagReadStreamController.close();
+
+    await _readerTriggerController.close();
+
+    await _readerInventoringController.close();
+
+    await _proximityController.close();
+  }
+
+  Future<bool> get isConnected async { 
+    bool result = await FlutterZebraRfid._channel.invokeMethod('getIsConnected', {
+      "reader": this.id
+    }) ?? false;
+    return result;
+  }
+
+  Future<void> connect({int maxRetries = 4}) async {
+    _tagReadStreamController = StreamController<TagData>.broadcast();
+    _readerTriggerController = StreamController<bool>.broadcast();
+    _readerInventoringController = StreamController<bool>.broadcast();
+    _proximityController = StreamController<int>.broadcast();
+
+    if (await isConnected) {
+      return;
+    }
+
+    _connectionCompleter = Completer();
+    await FlutterZebraRfid._channel.invokeMethod('connect', { 
+      "reader": this.id, 
+      "maxRetries": maxRetries
+    });
+    if (Platform.isIOS) {
+      await _connectionCompleter!.future;
+    }
+    else {
+      _isActive = true;
+    }
+  }
+
+  Future<String> startInventory({
+    required MemoryBank memoryBank, 
+    ReportConfig? reportConfig, 
+    AccessConfig? accessConfig
+  }) async {
+    final Map result = await FlutterZebraRfid._channel.invokeMethod('startInventory', { 
+      "reader": this.id, 
+      "memoryBank": memoryBank.key, 
+      "reportConfig": reportConfig == null ? null : {
+        "includeFirstSeenTime": reportConfig.includeFirstSeenTime, 
+        "includePhase": reportConfig.includePhase,
+        "includePC": reportConfig.includePC,
+        "includeRSSI": reportConfig.includeRSSI,
+        "includeChannelIndex": reportConfig.includeChannelIndex,
+        "includeLastSeenTime": reportConfig.includeLastSeenTime, 
+        "includeTagSeenCount": reportConfig.includeTagSeenCount
+      }, 
+      "accessConfig": accessConfig == null ? null : {
+        "doSelect": accessConfig.doSelect, 
+        "power": accessConfig.power
+      }
+    });
+    return result["status"];
+  }
+
+  Future<String> stopInventory() async {
+    final Map result = await FlutterZebraRfid._channel.invokeMethod('stopInventory', { 
+      "reader": this.id
+    });
+    return result["status"];
+  }
+
+  Future<String> startLocateTag(String tagEpcId) async {
+    final Map result = await FlutterZebraRfid._channel.invokeMethod('startLocateTag', { 
+      "reader": this.id, 
+      "tagEpcId": tagEpcId
+    });
+    _isLocating = true;
+    return result["status"];
+  }
+
+  Future<String> stopLocateTag() async {
+    final Map result = await FlutterZebraRfid._channel.invokeMethod('stopLocateTag', { 
+      "reader": this.id
+    });
+    _isLocating = false;
+    return result["status"];
+  }
+
+  Future<TagData> readTag({
+    required String tagId, 
+    required MemoryBank memoryBank, 
+    required int offset, 
+    required int length, 
+    int password = 0, 
+    int maxRetries = 10
+  }) async {
+    final result = await FlutterZebraRfid._channel.invokeMethod('readTag', { 
+      "reader": this.id, 
+      "tagId": tagId, 
+      "memoryBank": memoryBank.key, 
+      "offset": offset, 
+      "length": length, 
+      "password": password, 
+      "maxRetries": maxRetries
+    });
+    final tagData = TagData.fromMap(result["tagData"]);
+    return tagData;
+  }
+
+  Future<TagData> writeTag({
+    required String tagId, 
+    required MemoryBank memoryBank, 
+    required int offset, 
+    required Uint8List data, 
+    int password = 0, 
+    int maxRetries = 4
+  }) async {
+    final result = await FlutterZebraRfid._channel.invokeMethod('writeTag', { 
+      "reader": this.id, 
+      "tagId": tagId, 
+      "memoryBank": memoryBank.key, 
+      "offset": offset, 
+      "data": hex.encode(data.toList()), 
+      "password": password, 
+      "maxRetries": maxRetries
+    });
+    final tagData = TagData.fromMap(result["tagData"]);
+    return tagData;
+  }
+
+  void _eventCommunicationSessionEstablished() {
+    print("_eventCommunicationSessionEstablished: ${this.name}");
+  }
+
+  void _eventCommunicationSessionTerminated() {
+    print("_eventCommunicationSessionTerminated: ${this.name}");
+    _isActive = false;
+  }
+
+  void _eventCommunicationASCIIConnectionEstablished() {
+    print("_eventCommunicationASCIIConnectionEstablished: ${this.name}");
+    _isActive = true;
+    _connectionCompleter?.complete();
+  }
+
+  void _eventReadNotify(TagData tagData) {
+    _tagReadStreamController.add(tagData);
+  }
+
+  void _eventStatusNotify(EventStatus event, {EventStatusData? data}) {
+    print("eventStatusNotify: ${this.name} => $event => ${data?.toString()}");
+    switch (event) {
+      case EventStatus.operationStop:
+        _isInventoring = false;
+        _readerInventoringController.add(_isInventoring);
+        break;
+
+      case EventStatus.operationStart:
+        _isInventoring = true;
+        _readerInventoringController.add(_isInventoring);
+        break;
+
+      case EventStatus.operationBatchMode:
+        _isInventoring = true;
+        _readerInventoringController.add(_isInventoring);
+        break;
+      default:
+        break;
+    }
+  }
+
+  void _eventProximityNotify(int proximityPercent) {
+    print("eventProximityNotify: ${this.name} => $proximityPercent");
+    _proximityController.add(proximityPercent);
+  }
+
+  void _eventTriggerNotify(TriggerEvent event) {
+    print("_eventTriggerNotify: ${this.name} => $event");
+    _isTriggerPressed = (event == TriggerEvent.pressed);
+    _readerTriggerController.add(_isTriggerPressed);
+  }
+
+  void _eventBatteryNotify(String cause, int powerLevel, bool isCharging) {
+    print("_eventBatteryNotify: ${this.name} => cause=$cause, powerLevel=$powerLevel , isCharging=$isCharging");
+  }
+
+  void _eventDisconnectionNotify() {
+    print("_eventDisconnectionNotify");
+    _isActive = false;
+    onDisconnectedHandler?.call();
+  }
+}
