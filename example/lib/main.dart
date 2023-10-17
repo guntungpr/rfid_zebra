e931c654ca62962a9dd7e7ea858f19cf9f669422
guntungpr <guntungblinkerz123@gmail.com>
2023-10-17 11:24:00 +0700

    test push

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,7 +24,7 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
+  // Platform messages are asynchronous, so we initialize in an async method
   Future<void> initPlatformState() async {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
